f72bb08039feb7baf71559aeb5f113a77c836ad9
tianjing <tianjing@atguigu>
2022-12-10 09:55:43 +0800

    second commit

--- exer2/src/main/java/com/bj/Exer1.java
+++ exer2/src/main/java/com/bj/Exer1.java
@@ -6,6 +6,7 @@ package com.bj;
 public class Exer1 {
     public static void main(String[] args) {
         System.out.println("heihei");
+        System.out.println("版本2");
     }
     public void test01(){
         System.out.println("hahah");
