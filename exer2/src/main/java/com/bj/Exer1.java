182c3181586f34331bba6e4857c8e1742eddfcd2
tianjing <tianjing@atguigu>
2022-12-10 09:54:18 +0800

    first commit

---
+++ exer2/src/main/java/com/bj/Exer1.java
@@ -0,0 +1,13 @@
+package com.bj;
+
+/**
+ *
+ */
+public class Exer1 {
+    public static void main(String[] args) {
+        System.out.println("heihei");
+    }
+    public void test01(){
+        System.out.println("hahah");
+    }
+}
