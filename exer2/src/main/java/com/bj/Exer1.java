60dbad396faf1a2aac594a6d6cefe7bc1c9979e3
tianjing <tianjing@atguigu>
2022-12-10 09:56:18 +0800

    third commit

--- exer2/src/main/java/com/bj/Exer1.java
+++ exer2/src/main/java/com/bj/Exer1.java
@@ -7,6 +7,7 @@ public class Exer1 {
     public static void main(String[] args) {
         System.out.println("heihei");
         System.out.println("版本2");
+        System.out.println("版本3");
     }
     public void test01(){
         System.out.println("hahah");
